45753cf27a5bc3e870b60661032abf9566f03e6c
Danilo <60882968+TheDanOfCourse@users.noreply.github.com>
2022-08-20 20:21:11 -0300

    teste de commit

--- Parkfy/App.js
+++ Parkfy/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.texto}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  texto:{
+    color: 'white'
+  }
 });
