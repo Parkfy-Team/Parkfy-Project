11d98511ffb1951007ed6a278c267a9f01c00342
Danilo <60882968+TheDanOfCourse@users.noreply.github.com>
2022-08-21 23:25:39 -0300

    atualizacao tela inicial + navegacao

--- Parkfy/App.js
+++ Parkfy/App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import {
+import {ScrollView,
+  FlatList,
+  PixelRatio,
   StyleSheet,
   Text,
   View,
@@ -9,58 +11,61 @@ import {
   Button,
   TouchableOpacity,
 } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
+const Stack = createStackNavigator();
  
-export default function App() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
- 
+function App() {
   return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={require("./assets/logo.png")} />
-      <TouchableOpacity>
-        <Text style={styles.forgot_button}>A gente procura, você estaciona</Text>
-      </TouchableOpacity>
- 
-      <StatusBar style="auto" />
-
-      <TouchableOpacity style={styles.entrarBtn}>
-        <Text style={styles.entrarText}>Entrar</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.cadastrarBtn}>
-        <Text style={styles.cadastrarText}>Cadastre-se</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity>
-        <Text style={styles.visitanteText}>Acessar como visitante</Text>
-      </TouchableOpacity>
-      
-      {/* <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Email."
-          placeholderTextColor="#003f5c"
-          onChangeText={(email) => setEmail(email)}
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen 
+          name='Screen_Inicial'
+          component={Screen_Inicial}
         />
-      </View>
- 
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Password."
-          placeholderTextColor="#003f5c"
-          secureTextEntry={true}
-          onChangeText={(password) => setPassword(password)}
-        />
-      </View>     */}
- 
-     
-    </View>
-  );
+        <Stack.Screen 
+          name='Screen_Entrar'          
+          component={Screen_Entrar}
+        />            
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
  
-const styles = StyleSheet.create({
-  container: {
+function Screen_Inicial({navigation}){
+  const entrarButton = () => {
+    navigation.navigate('Screen_Entrar')
+  }
+  const cadastrarButton = () => {
+    navigation.navigate('Screen_Cadastrar')
+  }
+
+  return(
+  <View style={css_Inicial.body}>
+
+    <Image style={css_Inicial.image} source={require("./assets/logo.png")} />
+
+    <Text style={css_Inicial.nomeText}>Park<Text style={{color: 'white'}}>Fy</Text> </Text>
+
+    <Text style={css_Inicial.sloganText}>A gente procura, você estaciona</Text>
+
+    <TouchableOpacity onPress={entrarButton} style={css_Inicial.entrarBtn}>
+      <Text style={css_Inicial.entrarText}>Entrar</Text>
+    </TouchableOpacity>
+    
+    <TouchableOpacity onPress={cadastrarButton} style={css_Inicial.cadastrarBtn}>
+      <Text style={css_Inicial.cadastrarText}>Cadastre-se</Text>
+    </TouchableOpacity>
+
+    <TouchableOpacity>
+      <Text style={css_Inicial.visitanteText}>Acessar como visitante</Text>
+    </TouchableOpacity>
+  </View>
+  )
+}
+
+const css_Inicial = StyleSheet.create({
+  body: {
     flex: 1,
     backgroundColor: "black",
     alignItems: "center",
@@ -68,7 +73,11 @@ const styles = StyleSheet.create({
   },
  
   image: {
-    marginBottom: 40,
+    position:'absolute',
+    top: PixelRatio.roundToNearestPixel(141),
+    bottom: '55%',
+    width:PixelRatio.roundToNearestPixel(229),
+    height:PixelRatio.roundToNearestPixel(231),
   },
  
   inputView: {
@@ -76,8 +85,7 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     width: "70%",
     height: 45,
-    marginBottom: 20,
- 
+    marginBottom: 20, 
     alignItems: "center",
   },
  
@@ -86,6 +94,150 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     marginLeft: 20,
+  },
+  nomeText: {
+    position:'absolute',
+    color: '#2DCDB0',
+    top: '50%',
+    fontWeight: '400',
+    fontSize: PixelRatio.roundToNearestPixel(65),
+    lineHeight: PixelRatio.roundToNearestPixel(90),
+    textAlign: 'center',
+  },
+  sloganText: {
+    position:'absolute',
+    color: 'grey',
+    top: '50%',
+    fontWeight: '400',
+    fontSize: PixelRatio.roundToNearestPixel(20),
+    lineHeight: PixelRatio.roundToNearestPixel(24),
+    textAlign: 'center',
+  },
+ 
+  entrarBtn: {
+    display: 'flex',
+    flexDirection:"column",
+    alignItems:"center",
+    padding:(16,16),
+    position: "absolute",
+    height: PixelRatio.roundToNearestPixel(56),
+    bottom: PixelRatio.roundToNearestPixel(126),
+    width: PixelRatio.roundToNearestPixel(343),
+    backgroundColor: '#2DCDB0',
+    borderRadius: PixelRatio.roundToNearestPixel(100),    
+    
+  },
+  entrarText:{
+    width: PixelRatio.roundToNearestPixel(60),
+    height: PixelRatio.roundToNearestPixel(24),
+    color:'white',
+    fontWeight: '600',
+    fontSize: PixelRatio.roundToNearestPixel(20),
+    lineHeight: PixelRatio.roundToNearestPixel(24),
+    textAlign: 'center',  
+  },
+
+  cadastrarBtn: {
+    display: 'flex',
+    flexDirection:"column",
+    alignItems:'center',
+    padding:(16,16),
+    position: "absolute",
+    height: PixelRatio.roundToNearestPixel(56),
+    bottom: PixelRatio.roundToNearestPixel(208),
+    width: PixelRatio.roundToNearestPixel(343),
+    backgroundColor: '#2DCDB0',
+    borderRadius: PixelRatio.roundToNearestPixel(100),    
+  },
+
+  cadastrarText:{
+    width: PixelRatio.roundToNearestPixel(120),
+    height: PixelRatio.roundToNearestPixel(24),
+    color:'white',
+    fontWeight: '600',
+    fontSize: PixelRatio.roundToNearestPixel(20),
+    lineHeight: PixelRatio.roundToNearestPixel(24),
+    textAlign: 'center',
+  },
+
+  visitanteText:{
+    bottom: '10%',
+    height: PixelRatio.roundToNearestPixel(19),
+    bottom: PixelRatio.roundToNearestPixel(76),
+    fontWeight: '600',
+    fontSize: PixelRatio.roundToNearestPixel(20),
+    lineHeight: PixelRatio.roundToNearestPixel(19),
+    color:'#2DCDB0',
+    opacity: 0.7,
+    alignContent:'center',
+  }
+});
+
+function Screen_Entrar(){
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+ 
+  return (
+    <View style={css_Entrar.body}>
+
+      <View style={css_Entrar.inputView}>
+        <TextInput
+          style={css_Entrar.TextInput}
+          placeholder="Email."
+          placeholderTextColor="#003f5c"
+          onChangeText={(email) => setEmail(email)}
+        />
+      </View>
+ 
+      <View style={css_Entrar.inputView}>
+        <TextInput
+          style={css_Entrar.TextInput}
+          placeholder="Password."
+          placeholderTextColor="#003f5c"
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+        />
+      </View>
+ 
+      <TouchableOpacity>
+        <Text style={css_Entrar.forgot_button}>Forgot Password?</Text>
+      </TouchableOpacity>
+ 
+      <TouchableOpacity style={css_Entrar.loginBtn}>
+        <Text style={css_Entrar.loginText}>LOGIN</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+const css_Entrar = StyleSheet.create({
+  body: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+ 
+  image: {
+    marginBottom: 40,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+ 
+  inputView: {
+    backgroundColor: "#E8E8E8",
+    borderRadius: 30,
+    width: '70%',
+    height: 45,
+    marginBottom: 20, 
+    alignItems: "center",
+  },
+ 
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+    backgroundColor: '#E8E8E8',
   },
  
   forgot_button: {
@@ -129,3 +281,25 @@ const styles = StyleSheet.create({
     color: '#2DCDB0',
   }
 });
+
+
+{/* <View style={css_Inicial.inputView}>
+      <TextInput
+        style={css_Inicial.TextInput}
+        placeholder="Email."
+        placeholderTextColor="#003f5c"
+        onChangeText={(email) => setEmail(email)}
+      />
+    </View>
+
+    <View style={css_Inicial.inputView}>
+      <TextInput
+        style={css_Inicial.TextInput}
+        placeholder="Password."
+        placeholderTextColor="#003f5c"
+        secureTextEntry={true}
+        onChangeText={(password) => setPassword(password)}
+      />
+    </View>     */}
+
+export default App;
