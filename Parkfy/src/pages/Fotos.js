e50b22725c616dde76dfb0efe31c15a4e4549a77
Rodrigo Demeis <38820766+rdemeis@users.noreply.github.com>
2022-08-26 23:50:41 -0300

    photo request screen

--- Parkfy/src/pages/Fotos.js
+++ Parkfy/src/pages/Fotos.js
@@ -31,7 +31,23 @@ import {ScrollView,
   
     return(
     <>
-      <StatusBar barStyle={'light-content'}></StatusBar>        
+      <View style={css_Fotos.body}></View>
+      
+      <Text style={css_Fotos.quaseLaText}>Quase lá...</Text>
+        
+      <Text style={css_Fotos.fotoText}>Antes de seguir, por questões de{"\n"}segurança precisamos que você{"\n"}siga os passos a seguir:</Text>
+      
+      <Text style={css_Fotos.fotoDocRequestText}>1. Tire a foto de um documento de identificação válido (RG ou CNH)</Text>
+        
+      <TouchableOpacity style={css_Fotos.fotoDocBtn}>
+        <Text style={css_Fotos.fotoDocText}>Tirar Foto</Text>
+      </TouchableOpacity>
+
+      <Text style={css_Fotos.selfieRequestText}>2. Tire uma Selfie segurando{"\n"}o documento enquadrando o{"\n"}seu rosto (evite usar óculos, boné{"\n"}e outros objetos que dificultem a{"\n"}sua identificação)</Text>
+
+      <TouchableOpacity style={css_Fotos.selfieBtn}>
+        <Text style={css_Fotos.selfieText}>Tirar Foto</Text>
+      </TouchableOpacity>
     </>
     )
   }
--- Parkfy/src/styles/FotosStyle.js
+++ Parkfy/src/styles/FotosStyle.js
@@ -13,7 +13,107 @@ import {ScrollView,
 
 
 const css_Fotos = StyleSheet.create({
-
+    body: {
+        flex: 1,
+        backgroundColor: "black",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    
+    quaseLaText: {
+        fontFamily:"poppins_regular",
+        position:'absolute',
+        color: 'white',
+        top: PixelRatio.roundToNearestPixel(70),
+        left: PixelRatio.roundToNearestPixel(20),
+        fontWeight: '500',
+        fontSize: PixelRatio.roundToNearestPixel(40),
+        lineHeight: PixelRatio.roundToNearestPixel(45),
+    },
+    
+    fotoText: {
+        fontFamily:"poppins_regular",
+        position:'absolute',
+        color: 'grey',
+        top: PixelRatio.roundToNearestPixel(120),
+        left: PixelRatio.roundToNearestPixel(20),
+        fontWeight: '500',
+        fontSize: PixelRatio.roundToNearestPixel(20),
+        lineHeight: PixelRatio.roundToNearestPixel(40),
+        textAlign: 'justify',
+    },
+    
+    fotoDocRequestText: {
+        fontFamily:"poppins_regular",
+        position:'absolute',
+        color: 'grey',
+        top: PixelRatio.roundToNearestPixel(250),
+        left: PixelRatio.roundToNearestPixel(20),
+        fontWeight: '500',
+        fontSize: PixelRatio.roundToNearestPixel(20),
+        lineHeight: PixelRatio.roundToNearestPixel(40),
+        textAlign: 'justify',
+    },
+    
+    fotoDocBtn: {
+        display: 'flex',
+        flexDirection:"column",
+        alignItems: 'center',
+        padding:(16,16),
+        position: "absolute",
+        height: PixelRatio.roundToNearestPixel(56),
+        top: PixelRatio.roundToNearestPixel(345),
+        width: PixelRatio.roundToNearestPixel(343),
+        backgroundColor: '#2DCDB0',
+        borderRadius: PixelRatio.roundToNearestPixel(100),
+        marginLeft: PixelRatio.roundToNearestPixel(25)
+    },
+    
+    fotoDocText: {
+        width: PixelRatio.roundToNearestPixel(80),
+        height: PixelRatio.roundToNearestPixel(24),
+        color:'white',
+        fontWeight: '600',
+        fontSize: PixelRatio.roundToNearestPixel(18),
+        lineHeight: PixelRatio.roundToNearestPixel(24),
+        textAlign: 'center',
+    },
+    
+    selfieRequestText: {
+        fontFamily:"poppins_regular",
+        position:'absolute',
+        color: 'grey',
+        top: PixelRatio.roundToNearestPixel(420),
+        left: PixelRatio.roundToNearestPixel(20),
+        fontWeight: '500',
+        fontSize: PixelRatio.roundToNearestPixel(20),
+        lineHeight: PixelRatio.roundToNearestPixel(40),
+        textAlign: 'justify',
+    },
+    
+    selfieBtn: {
+        display: 'flex',
+        flexDirection:"column",
+        alignItems: 'center',
+        padding:(16,16),
+        position: "absolute",
+        height: PixelRatio.roundToNearestPixel(56),
+        bottom: PixelRatio.roundToNearestPixel(90),
+        width: PixelRatio.roundToNearestPixel(343),
+        backgroundColor: '#2DCDB0',
+        borderRadius: PixelRatio.roundToNearestPixel(100),
+        marginLeft: PixelRatio.roundToNearestPixel(25)
+    },
+    
+    selfieText: {
+        width: PixelRatio.roundToNearestPixel(80),
+        height: PixelRatio.roundToNearestPixel(24),
+        color:'white',
+        fontWeight: '600',
+        fontSize: PixelRatio.roundToNearestPixel(18),
+        lineHeight: PixelRatio.roundToNearestPixel(24),
+        textAlign: 'center',
+    }
 
 });
 
