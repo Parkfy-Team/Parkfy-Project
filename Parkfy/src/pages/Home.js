c10f9c0c4afe1239a81bef183efbe03dc548e4b1
Danilo <60882968+TheDanOfCourse@users.noreply.github.com>
2022-08-29 23:41:48 -0300

    update home e cadastro

--- Parkfy/src/pages/Home.js
+++ Parkfy/src/pages/Home.js
@@ -1,39 +1,89 @@
 import {ScrollView,
-    SafeAreaView,
-    FlatList,
-    PixelRatio,
-    StyleSheet,
-    Text,
-    View,
-    Image,
-    TextInput,
-    Button,
-    TouchableOpacity,
-  } from "react-native";
-  
-  import { NavigationContainer } from '@react-navigation/native';
-  import { createStackNavigator } from "@react-navigation/stack";
-  const Stack = createStackNavigator();
-  import { StatusBar, TurboModuleRegistry } from 'react-native';
-  import { useFonts } from 'expo-font';
-  import React, { useState } from "react";
+  SafeAreaView,
+  FlatList,
+  PixelRatio,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+import { Picker } from '@react-native-picker/picker'
+import MaskInput,{ Masks } from 'react-native-mask-input';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
+const Stack = createStackNavigator();
+import { StatusBar, TurboModuleRegistry } from 'react-native';
+import { useFonts } from 'expo-font';
+import React, { useState } from "react";
   
   import {css_Home} from '../styles/HomeStyle.js';
+  import {css_Cadastro} from '../styles/CadastroStyle.js';
+
 
 
   function Screen_Home({navigation}){
 
-    const entrarButton = () => {
-      navigation.navigate('Screen_Entrar')
-    }
-    const cadastrarButton = () => {
-      navigation.navigate('Screen_Cadastrar')
-    }
+    const [primeiroNome, setPrimeiroNome] = useState('');
+  const [sobrenome, setSobrenome] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [senha, setSenha] = useState("");
+  const [telefone, setTelefone] = useState('');
+  const [datanasc, setDatanasc] = useState('');
+  const [cpf, setCPF] = useState('');
+  const [isSecureEntry,setIsSecureEntry] = useState(true);
+
+  const entrarButton = () => {
+    navigation.navigate('Screen_Entrar')
+  }    
+  const closeButton = () => {
+    navigation.navigate('Screen_Inicial')
+  }  
+
+  const cadastrarBtn = () => {
+    navigation.navigate('Screen_Fotos')
+  }
+
+  const [genero, setGenero] = useState("");
   
     return(
     <>
-      <StatusBar barStyle={'light-content'}></StatusBar>        
-    </>
+      <StatusBar backgroundColor={'white'} barStyle={'dark-content'}></StatusBar>
+    <KeyboardAwareScrollView  keyboardShouldPersistTaps={'always'}
+      style={{flex:1, backgroundColor:'white'}}
+      showsVerticalScrollIndicator={false}>
+    <View style={css_Cadastro.body}>    
+
+        <View style={css_Cadastro.textoPrincipalView}>
+          <Text style={css_Cadastro.textoPrincipalText}>Vagas</Text>
+        </View>      
+
+        <View style={css_Cadastro.boxColumn}>
+            <TextInput
+            style={css_Cadastro.textColumn}
+            placeholder="Buscar Vaga"
+            placeholderTextColor="#BDBDBD"
+            />
+            <TouchableOpacity
+              style={css_Cadastro.mostrarSenhaView}
+              onPress={(event) => {((prev) => !prev);}}
+            >
+              <Text style={css_Cadastro.mostrarSenhaText}>Filtrar</Text>
+            </TouchableOpacity>
+          </View>    
+          
+            <TouchableOpacity style={css_Cadastro.cadastroBtn} onPress={cadastrarBtn}>
+              <Text style={css_Cadastro.cadastroText}>arrumar</Text>
+            </TouchableOpacity>
+
+
+        </View>
+    </KeyboardAwareScrollView>    
+  </>
     )
   }
   
--- Parkfy/src/styles/HomeStyle.js
+++ Parkfy/src/styles/HomeStyle.js
@@ -13,7 +13,77 @@ import {ScrollView,
 
 
 const css_Home = StyleSheet.create({
+    body:{
+        flex: 1,
+        backgroundColor: "white",
+    },
+    container:{
+      flexDirection:'column',
+      position:'relative',
+      alignItems: 'flex-start',
+      width: PixelRatio.roundToNearestPixel(343),
+    },
+    textoPrincipalText: {
+      fontFamily:'Inter-Regular',
+      position: 'absolute',
+      fontSize: PixelRatio.roundToNearestPixel(30),
+      fontWeight: '700',
+      fontStyle: "normal",
+      lineHeight: 30,
+      textAlign: "center",
+      color: "#000000",
+    },
+    
+    textoPrincipalView: {
+      alignItems: "center",
+      top: 20,
+    },
+    
+    boxColumn: {
+      borderColor:'#E8E8E8',
+      borderWidth:1,
+      width: PixelRatio.roundToNearestPixel(343),
+      backgroundColor:'#F6F6F6',
+      borderRadius: PixelRatio.roundToNearestPixel(8),
+      top: PixelRatio.roundToNearestPixel(70),
+      left: PixelRatio.roundToNearestPixel(18),
+      height: PixelRatio.roundToNearestPixel(50),
+      margin: 5,
+    },
+    
+    textColumn: {
+      position:'absolute',
+      fontSize: 16,
+      fontWeight: "500",
+      fontStyle: "normal",
+      lineHeight: 16,
+      marginLeft: 10,
+      height: 45,
+    },
 
+    closeBtn: {
+        marginTop:1,
+        width: PixelRatio.roundToNearestPixel(16),
+        height: PixelRatio.roundToNearestPixel(10),
+        marginLeft: PixelRatio.roundToNearestPixel(20),
+        top: PixelRatio.roundToNearestPixel(30),
+    },
+    filtrarView: {
+        position:'absolute',
+        marginTop: 10,
+        height: PixelRatio.roundToNearestPixel(30),
+        width: PixelRatio.roundToNearestPixel(80),
+        alignItems:'center',    
+        marginLeft: PixelRatio.roundToNearestPixel(270),
+      },
+      
+    filtrarText: {
+        marginTop:3,
+        fontSize: 15,
+        position: "absolute",
+        color: "#2DCDB0",
+        marginRight: 10,
+    }, 
 
 });
 
